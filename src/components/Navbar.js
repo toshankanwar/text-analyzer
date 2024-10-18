6ba1bd375912cc7d8610832519eb6958dfd5260b
toshan kanwar <162002505+toshankanwar@users.noreply.github.com>
2024-10-18 23:34:01 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <NavLink className="nav-link active" aria-current="page"to="/">Home</NavLink>
+          <NavLink className="nav-link active" aria-current="page"to="/text-analyzer">Home</NavLink>
         </li>
      
   
